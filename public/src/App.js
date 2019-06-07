b8df3358495f7a020648b94f84177d996ca86baa
Kenny <kennethpolyak@gmail.com>
2019-06-07 17:24:52 -0400

    Setup mock data

--- public/src/App.js
+++ public/src/App.js
@@ -3,6 +3,28 @@ import NotificationList from "./components/NotificationList"
 import { hot } from "react-hot-loader"
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      notifications: []
+    };
+    this.fetchNotifications = this.fetchNotifications.bind(this);
+  }
+
+  async fetchNotifications() {
+    try {
+      const res = await fetch("http://localhost:4433/availabilities");
+      const json = await res.json();
+      this.setState({availabilities: json.reverse()});
+    } catch (e) {
+      console.error("Failed to fetch 'notifications' data", e);
+    }
+  }
+
+  componentDidMount() {
+    this.fetchNotifications();
+  }
+
   render() {
     return (
       <div className="app">
---
+++ server/data/mockNotifications.md
@@ -0,0 +1,10 @@
+# Notifications
+## New Thing 1
+### 7/16/2018
+About this
+## New Thing 2
+### 7/14/2018
+About this
+## New Thing 2
+### 7/12/2018
+About this
