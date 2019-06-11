f819d6a4b819cd8c4a2b948e111aae1a13272349
Kenny <kennethpolyak@gmail.com>
2019-06-11 17:18:51 -0400

    Basic modal notifications layour

--- public/src/components/NotificationsList.js
+++ public/src/components/NotificationsList.js
@@ -4,14 +4,14 @@ import NotificationsListItem from "./NotificationsListItem";
 class NotificationsList extends React.Component {
   render(props) {
     return (
-      <div className="changelog-list-container">
+      <div className="notifications-list-container">
         {this.props.notifications.map((notification, i) => {
           return (
             <NotificationsListItem
-            key={i}
-            title={notification.title}
-            date={notification.date}
-            body={notification.body}
+              key={i}
+              title={notification.title}
+              date={notification.date}
+              body={notification.body}
             />
           )
         })}
--- public/src/components/NotificationsListItem.js
+++ public/src/components/NotificationsListItem.js
@@ -3,7 +3,7 @@ import React from "react";
 class NotificationsListItem extends React.Component {
   render(props) {
     return (
-      <div>
+      <div className="notifications-list-item-container">
         <div>{this.props.title}</div>
         <div>{this.props.body}</div>
       </div>
