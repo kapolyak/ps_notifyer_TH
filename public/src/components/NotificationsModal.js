b1c513038cdd06a8326c47e9cae4a7af3bdb715f
Kenny <kennethpolyak@gmail.com>
2019-06-14 14:23:55 -0400

    Replace svg with correct img for arrow

--- public/src/components/NotificationsModal.js
+++ public/src/components/NotificationsModal.js
@@ -51,9 +51,7 @@ class NotificationsModal extends React.Component {
                 <div className="card" key={"view"}>
                   <div className="header">
                     <div onClick={this.renderLatestChanges} className="arrow-container">
-                      <svg className="arrow" viewBox="0 0 18 18" role="presentation" aria-hidden="true" focusable="false">
-                        <path d="m16.29 4.3a1 1 0 1 1 1.41 1.42l-8 8a1 1 0 0 1 -1.41 0l-8-8a1 1 0 1 1 1.41-1.42l7.29 7.29z" fillRule="evenodd"></path>
-                      </svg>
+                      <img width="100%" src="https://img.icons8.com/ios/50/000000/back.png" />
                     </div>
                     <h2>{this.props.notifications[this.state.selectedNotificationIndex].title}</h2>
                     <div className="header-balance-invisible"></div>
